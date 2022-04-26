f30cdc22cc22bbb0955a2b05f1f8318d095164d4
caryaharper <caryaharper@gmail.com>
2022-04-26 15:38:46 -0500

    Added routing logic

--- routes/api.js
+++ routes/api.js
@@ -9,10 +9,43 @@ module.exports = function (app) {
   app.route('/api/check')
     .post((req, res) => {
 
+      try {
+        res.json(solver.checkPlacement(req.body));
+      } catch(e) {
+        res.json({ error: e.message });
+        return;
+      }
+      
     });
     
   app.route('/api/solve')
     .post((req, res) => {
+      const {puzzle} = req.body;
+      
+      if (puzzle === undefined) {
+        res.json({ error: 'Required field missing' });
+        return;
+      }
 
+      if (typeof puzzle !== 'string') {
+        res.json({ error: 'Required field type must be string'});
+        return;
+      }
+
+      try {
+        solver.validatePuzzle(req.body);
+      } catch(e) {
+        res.json({ error: e.message});
+        return;
+      }  
+      const solution = solver.solve(puzzle);
+
+      if (solution === 1) {
+        res.json({ error: 'Puzzle cannot be solved' });
+        return;
+      }
+      
+      res.json({solution});
+      return;
     });
 };
